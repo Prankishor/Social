673e7e6024dcdbb224a7a42f7db21aecd8c20a91
Prankishor <33565966+Prankishor@users.noreply.github.com>
2023-11-08 22:12:31 +0530

    set up configs server

---
+++ server/index.js
@@ -0,0 +1,70 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
+
+/*CONFIGURATIONS (MIDDLESWARES)*/
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+
+/*FILE STORAGE with MULTER in LOCAL*/
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "public/assets");
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({ storage: storage });
+
+
+
+
+
+
+/* ROUTES WITH FILES FOR AUTHENTICATION*/
+
+//-> "/auth/register" is a endpoint that will be hit when user is registering first. Before register 
+//logic is run, it goes through middleware "upload" which will store profile picture in local and
+//register is just the logic (function) that will run when this end point is hit. Register logic 
+//is written in auth.js inside controllers directory, so we are importing it from there.
+app.post("/auth/register", upload.single("picture"), register)
+
+
+
+
+
+
+
+
+/*MONGOOSE SETUP*/
+
+const PORT = process.env.PORT || 6001
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+})
+    .catch((error) => console.log(`${error} did not connect`))
